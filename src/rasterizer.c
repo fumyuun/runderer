f7d4d87182adc42b299d7e5b5cb3d60007a2076e
Nick Overdijk <nick@astrant.net>
2016-04-05 22:44:04 +0200

    Fixes warnings

--- src/rasterizer.c
+++ src/rasterizer.c
@@ -4,11 +4,9 @@
 #include "math.h"
 
 void rasterize_triangle(struct runderer* self, stream_t p1, stream_t p2, stream_t p3, fragment_t* frag_buf_begin, fragment_t** frag_buf_end) {
-    vec3f_t p;       // the current point to consider rasterizing
     vec3f_t bc;      // the barycentric cordinate of p with respect to triangle p1p2p3
     vec3f_t normal;  // the face normal of triangle p1p2p3
     float intensity;    // the light intensity
-    unsigned int color_shaded;  // the shaded color, linearly interpolated over intensity (for now)
 
     // compute and normalize our face normal
     math_normal(p1.position, p2.position, p3.position, normal);
@@ -26,8 +24,8 @@ void rasterize_triangle(struct runderer* self, stream_t p1, stream_t p2, stream_
     fragment_t* frag_buf_current = frag_buf_begin;
 
     // iterate over all points to consider if they're in the triangle
-    for (int y = 0; y < self->framebuffer->height; ++y) {
-        for (int x = 0; x < self->framebuffer->width; ++x) {
+    for (uint y = 0; y < self->framebuffer->height; ++y) {
+        for (uint x = 0; x < self->framebuffer->width; ++x) {
             fragment_t frag = {
                 .screen = {x, y, 0},
                 .color = {intensity * p1.color[0], intensity * p1.color[1], intensity * p1.color[2], 1.0f}
